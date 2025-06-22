1218f1cd543baa86cb79f4a933d7a552b53bd4db
Lotrando <miroslav.klika@seznam.cz>
2025-06-22 20:06:05 +0200

    firmfware binary kernel fix

--- installer.sh
+++ installer.sh
@@ -341,25 +341,25 @@ input_settings() {
         read -p "$(echo -e "${BLUE}Choose kernel type (1-4):${RESET} ")" kernel_choice
         case "$kernel_choice" in
             1)
-                GENTOO_KERNEL="zen-sources"
+                GENTOO_KERNEL="zen-sources linux-firmware"
                 KERNEL_NAME="Zen Sources"
                 echo -e "You have chosen: ${GREEN}${KERNEL_NAME}${RESET}"
                 break
                 ;;
             2)
-                GENTOO_KERNEL="git-sources"
+                GENTOO_KERNEL="git-sources linux-firmware"
                 KERNEL_NAME="Git Sources"
                 echo -e "You have chosen: ${GREEN}${KERNEL_NAME}${RESET}"
                 break
                 ;;
             3)
-                GENTOO_KERNEL="gentoo-sources"
+                GENTOO_KERNEL="gentoo-sources linux-firmware"
                 KERNEL_NAME="Gentoo Sources"
                 echo -e "You have chosen: ${GREEN}${KERNEL_NAME}${RESET}"
                 break
                 ;;
             4)
-                GENTOO_KERNEL="gentoo-kernel-bin"
+                GENTOO_KERNEL="linux-firmware gentoo-kernel-bin"
                 KERNEL_NAME="Gentoo Sources Binary"
                 echo -e "You have chosen: ${GREEN}${KERNEL_NAME}${RESET}"
                 break
@@ -902,7 +902,7 @@ source /etc/profile >/dev/null 2>&1
 
 # Kernel and packages
 emerge ${GENTOO_KERNEL}
-emerge linux-firmware genkernel && genkernel all
+emerge genkernel && genkernel all
 emerge f2fs-tools dosfstools grub terminus-font sudo
 
 # GRUB configuration
